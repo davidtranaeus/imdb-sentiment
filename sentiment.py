1a888c0561a00c6ebfa7733b1ffe1f2468626f7c
davidtranaeus <davidtra@kth.se>
2018-12-12 15:26:34 +0100

    Add stuff

--- sentiment.py
+++ sentiment.py
@@ -21,7 +21,6 @@ def tf_idf(samples):
 
 def binary(samples):
   """one hot encoding representation"""
-  print('binary')
   return (samples > 0).astype(np.int)
 
 def less_data(data, n_of_each = 5000):
@@ -35,50 +34,54 @@ def less_data(data, n_of_each = 5000):
 
   return less
 
-if __name__ == "__main__":
-  
-  print('Reading training data')
-  samples, targets_data, vocabulary = trainData()
-  samples = samples[:-2]
-
-  print('Extracting targets')
-  targets = targets_data[:,1].reshape(-1,1)
-  review_no = targets_data[:,0]
-
-  print('Cropping training data')
-  training_subset = 5000
-  less_samples = less_data(samples, training_subset)
-  less_targets = less_data(targets, training_subset)
+def prepare_data(samples, targets, subset=1000, tfidf=False, binary=False):
+  print('Cropping data')
+  training_subset = subset
+  less_samples = less_data(samples, subset)
+  less_targets = less_data(targets, subset)
   less_targets = np.ravel(less_targets)
 
-  # X_train, X_val, y_train, y_val = train_test_split(
-  #   less_samples, less_targets, train_size = 0.75)
+  if binary:
+    print('Setting binary values')
+    less_samples = binary(less_samples)
   
-  # for c in [0.01, 0.05, 0.25, 0.5, 1]: # regularization paramters
-  #   lr = LogisticRegression(C=c) 
-  #   lr.fit(X_train, y_train)
-  #   score = lr.score(X_val, y_val)
-  #   print(score)
+  if tfidf:
+    print('Setting tf-idf values')
+    less_samples = tf_idf(less_samples)
+
+  return less_samples, less_targets
+  
+if __name__ == "__main__":
+  
+  is_binary = False
+  is_tfidf = False
+  subset = 1000
+
+  print('Reading training data')
+  train_samples, train_targets_data, vocabulary = trainData()
+  train_samples = train_samples[:-2]
+  train_targets = train_targets_data[:,1].reshape(-1,1)
+  train_samples, train_targets = prepare_data(
+    train_samples, 
+    train_targets,
+    subset=subset,
+    tfidf=is_tfidf,
+    binary=is_binary)
 
   print('Training logistic regression')
   lr = LogisticRegression() 
-  lr.fit(less_samples, less_targets)
+  lr.fit(train_samples, train_targets)
 
   print('Reading test data')
   test_samples, test_targets_data = testData(vocab = vocabulary)
-  print(test_samples.shape)
   test_samples = test_samples[:-2]
-  n_test_samples = test_samples.shape[0]
-
-  print('Extracting targets')
   test_targets = test_targets_data[:,1].reshape(-1,1)
-  review_no = test_targets_data[:,0]
-
-  print('Cropping test data')
-  test_subset = 5000
-  less_test_samples = less_data(test_samples, test_subset)
-  less_test_targets = less_data(test_targets, test_subset)
-  less_test_targets = np.ravel(less_test_targets)
+  test_samples, test_targets = prepare_data(
+    test_samples, 
+    test_targets,
+    subset=subset,
+    tfidf=is_tfidf,
+    binary=is_binary)
 
   print('Accuracy: ', end='')
-  print(lr.score(less_test_samples, less_test_targets))
+  print(lr.score(test_samples, test_targets))
